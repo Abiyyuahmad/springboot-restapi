05733fd69cdcc0c671d91a181429645db1171bec
Bustomi Raharjo <bustomi.raharjo@tokopedia.com>
2019-12-16 16:18:22 +0700

    logger interceptor

--- src/main/java/com/digimaster/ApiMvcAdapter.java
+++ src/main/java/com/digimaster/ApiMvcAdapter.java
@@ -1,4 +1,14 @@
 package com.digimaster;
 
-public class ApiMvcAdapter {
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class ApiMvcAdapter extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ApiLoggerInterceptor());
+    }
 }
